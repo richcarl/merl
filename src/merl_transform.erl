9c7d3a16dc753d9c13cdb77fbf28a653c53e2e81
Richard Carlsson <richardc@klarna.com>
2013-06-30 00:46:24 +0200

    split traversal into pre- and post-transform

--- src/merl_transform.erl
+++ src/merl_transform.erl
@@ -8,19 +8,39 @@
 
 -export([parse_transform/2]).
 
-%% NOTE: We cannot use inline metavariables in this module, because it must
-%% be possible to compile it with the parse transform disabled!
+%% NOTE: We cannot use inline metavariables or any other parse transform
+%% features in this module, because it must be possible to compile it with
+%% the parse transform disabled!
 -include("../include/merl.hrl").
-
 
 %% TODO: unroll calls to switch? it will probably get messy
 
 parse_transform(Forms, _Options) ->
-    erl_syntax:revert_forms(
-      erl_syntax_lib:map(fun (T) -> transform(T) end,
-                         erl_syntax:form_list(Forms))).
+    erl_syntax:revert_forms(map(erl_syntax:form_list(Forms))).
 
-transform(T) ->
+map(Tree0) ->
+    Tree = pre(Tree0),
+    post(case erl_syntax:subtrees(Tree) of
+             [] ->
+                 Tree;
+             Gs ->
+                 erl_syntax:update_tree(Tree, [[map(T) || T <- G] || G <- Gs])
+         end).
+
+pre(T) ->
+    merl:switch(
+      T,
+      [{?Q("merl:quote(_@line, _@text) = _@expr"),
+        fun ([{expr, _}, {line, _}, {text,Text}]) ->
+                erl_syntax:is_literal(Text)
+        end,
+        fun ([{expr, _}, {line, _}, {text, Text}]=Env) ->
+                expand_match(Text, Env, T)
+        end},
+       fun () -> T end
+      ]).
+
+post(T) ->
     merl:switch(
       T,
       [{?Q("merl:_@function(_@@args)"),
@@ -43,6 +63,7 @@ transform(T) ->
        fun () -> T end]).
 
 expand_quote([StartPos, Text, Env], T, _) ->
+    %% TODO: use switch guard instead of is_literal test here
     case erl_syntax:is_literal(StartPos) of
         true ->
             expand_quote([Text, Env], T, erl_syntax:concrete(StartPos));
@@ -50,18 +71,20 @@ expand_quote([StartPos, Text, Env], T, _) ->
             T
     end;
 expand_quote([Text, Env], T, StartPos) ->
+    %% TODO: use switch guard instead of is_literal test here
     case erl_syntax:is_literal(Text) of
         true ->
             As = [StartPos, erl_syntax:concrete(Text)],
             % keep expanding if possible
-            transform(?Q("merl:subst(_@tree, _@env)",
-                         [{tree, eval_call(quote, As, T)},
-                          {env, Env}]));
+            map(?Q("merl:subst(_@tree, _@env)",
+                   [{tree, eval_call(quote, As, T)},
+                    {env, Env}]));
         false ->
             T
     end.
 
 expand_template(F, [Pattern | Args], T) ->
+    %% TODO: use switch guard instead of is_literal test here
     case erl_syntax:is_literal(Pattern) of
         true ->
             As = [erl_syntax:concrete(Pattern)],
@@ -94,6 +117,11 @@ eval_call(F, As, T) ->
     catch
         throw:_Reason -> T
     end.
+
+expand_match(Text, Env, _T) ->
+    _Text1 = erl_syntax:concrete(Text),
+    %% TODO: must get variables from Text and match on output env
+    map(?Q("{ok, 42} = merl:match(merl:quote(_@line, _@text), _@expr)", Env)).
 
 is_inline_metavar(Var) when is_atom(Var) ->
     is_erlang_var(atom_to_list(Var));
