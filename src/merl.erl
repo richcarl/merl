0b0e5f74055b1f5092f54bcc6b1151de6773e4e8
Richard Carlsson <richardc@klarna.com>
2014-02-05 15:49:51 +0100

    fix doc breakage

--- src/merl.erl
+++ src/merl.erl
@@ -1,8 +1,4 @@
 %% ---------------------------------------------------------------------
-%% @author Richard Carlsson <carlsson.richard@gmail.com>
-%% @copyright 2010-2013 Richard Carlsson
-%% @doc Metaprogramming in Erlang.
-%%
 %% Licensed under the Apache License, Version 2.0 (the "License"); you may
 %% not use this file except in compliance with the License. You may obtain
 %% a copy of the License at <http://www.apache.org/licenses/LICENSE-2.0>
@@ -12,6 +8,10 @@
 %% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
+%%
+%% @author Richard Carlsson <carlsson.richard@gmail.com>
+%% @copyright 2010-2013 Richard Carlsson
+%% @doc Metaprogramming in Erlang.
 
 -module(merl).
 
